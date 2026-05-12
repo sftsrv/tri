074b6855d306ba4d78d5a1eff44c9b82b97ea972
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2026-05-12 09:20:11 +0200

    :bug: ensure that commands without a placeholder still work

--- command/command.go
+++ command/command.go
@@ -38,6 +38,11 @@ func generateCommand(base string, pattern string, input string) (bin string, arg
 	// Replace lone $ only after all others have been done.
 	// This is a convenience syntax for $0
 	result = strings.ReplaceAll(result, "$", input)
+
+	if result == base {
+		result = base + " " + input
+	}
+
 	if len(result) > 0 {
 		parts := strings.Split(result, " ")
 		bin := parts[0]
